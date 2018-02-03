7dc0031ea48b634c222ae041b94aa8385dbbd03d
Mateusz <gorzanski.mateusz@gmail.com>
2018-02-03 12:33:04 +0100

    add basic cards for notes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import NotesList from './components/NotesList';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
+        <main className="main">
+          <NotesList />
+        </main>
       </React.Fragment>
     );
   }
